2f365379c312dcfc6cbd9a9f0e72f0e103968d76
José Everton <vetophp@gmail.com>
2020-08-09 19:54:52 -0300

    Exemplo HandPose

---
+++ HandPose/camera.js
@@ -0,0 +1,39 @@
+function Camera() {
+     this.videoTag = document.querySelector('video')
+     this.videoConfig = { audio: false, video: { width: this.videoTag.width, height: this.videoTag.height } }
+}
+
+Camera.prototype = {
+     start: function () {
+          this.ligado = true
+
+          navigator.mediaDevices.getUserMedia(this.videoConfig).then(function (mediaStream) {
+               this.videoTag = document.querySelector('video');
+               this.videoTag.srcObject = mediaStream;
+          }).catch(function (err) {
+               console.log("Não foi possível iniciar o vídeo" + err);
+          })
+     },
+     
+     stop: function () {
+          this.ligado = false
+          const strem = this.videoTag.srcObject
+          const tracks = strem.getTracks();
+
+          tracks.forEach(function (track) {
+               track.stop();
+          });
+
+          this.videoTag.srcObject = null;
+     },
+
+     async estimateHandsPoseOnVideo() {
+          const model = await handpose.load();
+          await model.estimateHands(this.videoTag, { flipHorizontal: false }).then(result => {
+               this.resultPoses = result;
+          }).catch((err) => {
+               console.log('Falha ao analisar', err);
+          });
+          return this.resultPoses;
+     }
+}
